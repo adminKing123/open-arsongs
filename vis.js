77770045fa5630e9cd8947dc5cc90e4c88f2c146
Harsh Narwariya <h.narwariya@thesynapses.com>
2024-06-26 19:30:01 +0530

    visualizer changes

--- vis.js
+++ vis.js
@@ -1,9 +1,32 @@
-// anaylizer here
-
 import AudioMotionAnalyzer from "https://cdn.skypack.dev/audiomotion-analyzer?min";
 const audioMotion = new AudioMotionAnalyzer(
   document.getElementById("visualizer-container"),
   {
     source: document.getElementById("player"),
+    source: document.getElementById("player"),
+    mode: 5,
+    fftSize: 8192,
+    minFreq: 20,
+    maxFreq: 22000,
+    barSpace: 0.1,
+    ledBars: true,
+    showBgColor: true,
+    overlay: true,
+    channelLayout: "single",
+    radial: false,
+    reflexRatio: 0,
+    linearAmplitude: true,
+    linearBoost: 1.8,
+    ledSpaceRatio: 0.2,
+    colorMode: "gradient",
+    gradient: "rainbow",
+    lumiBars: false,
+    showScaleX: false,
+    showPeaks: false,
+    smoothing: 0.7,
+    fillAlpha: 1,
+    bgAlpha: 0.7,
+    useCanvas: true,
+    frequencyScale: "bark",
   }
 );
